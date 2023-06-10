8ae169a6f36d7d440fe5cb1c0864f81ee509e059
Harini Mohan <128628820+kaizenchan@users.noreply.github.com>
2023-06-10 23:57:21 +0530

    changed maxSum variable; different on CN & LC
    0/INT_MIN anyone explain? pls

--- 2_Kadane'sAlgo(MSS).cpp
+++ 2_Kadane'sAlgo(MSS).cpp
@@ -15,3 +15,17 @@ public:
         return maxSum;
     }
 };
+
+// CN CODESTUDIO 0 INSTEAD OF INT_MIN
+long long maxSubarraySum(int nums[], int n)
+{
+        long currentSum=0; long maxSum=0;
+        for(int i=0; i<n; i++){
+            currentSum+=nums[i];
+            if(currentSum > maxSum)
+                maxSum=currentSum;
+            if(currentSum < 0)
+                currentSum=0;
+        }
+        return maxSum;
+}
