d4e19c1ff5a6b681d0ca5a3df406faf2b6863855
Harini Mohan <128628820+kaizenchan@users.noreply.github.com>
2023-07-11 22:11:58 +0530

    Update 20_GridUniquePaths.cpp

--- 20_GridUniquePaths.cpp
+++ 20_GridUniquePaths.cpp
@@ -1,13 +1,12 @@
-class Solution {
-public:
-    int recursive(int i, int j, int n, int m, vector<vector<int>> &dp){
-        if(i==n-1 && j==m-1) return 1;
-        if(i>=n || j>=m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=recursive(i+1,j,n,m,dp)+recursive(i,j+1,n,m,dp);
-    }
-    int uniquePaths(int n, int m) {
-        vector<vector<int>> dp (n, vector<int> (m, -1));
-        return recursive(0,0,n,m,dp);
-    }
-};
+int uniquePaths(int m, int n) {
+	int dp[m][n];
+	for(int i=0; i<m; i++){
+		for(int j=0; j<n; j++){
+			if(i==0 || j==0)
+				dp[i][j]=1;
+			else
+				dp[i][j]=dp[i-1][j]+dp[i][j-1];
+		}
+	}
+	return dp[m-1][n-1];
+}
